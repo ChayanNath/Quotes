5ee7c7ec18cae3d6e2c81a6d5438dbdb5d3ee1ce
chayanNath <chayannath19@gmail.com>
2023-01-02 18:38:57 +0530

    Added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
+import { lazy, Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-import MainNavigation from "./components/layout/MainNavigation";
-import AddNewQuote from "./pages/AddNewQuote";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 import AllQuotes from "./pages/AllQuotes";
-import NotFound from "./pages/NotFound";
-import QuoteDetails from "./pages/QuoteDetails";
+
+const AddNewQuote = lazy(() => import("./pages/AddNewQuote"));
+const QuoteDetails = lazy(() => import("./pages/QuoteDetails"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes"></Redirect>
-        </Route>
-        <Route path="/quotes" exact>
-          <AllQuotes></AllQuotes>
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetails></QuoteDetails>
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetails></QuoteDetails>
-        </Route>
-        <Route path="/new-quote">
-          <AddNewQuote></AddNewQuote>
-        </Route>
-        <Route path="*">
-          <NotFound></NotFound>
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes"></Redirect>
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes></AllQuotes>
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetails></QuoteDetails>
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetails></QuoteDetails>
+          </Route>
+          <Route path="/new-quote">
+            <AddNewQuote></AddNewQuote>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
